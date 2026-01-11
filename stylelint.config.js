0b5397a5d1c2c1821c01a49bf2826d41c975fb63
SaekiTominaga <kcsu3470+github@gmail.com>
2026-01-11 10:24:20 +0900

    @w0s/stylelint-config@4.17 にアップデート

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,8 @@ export default {
 	rules: {
 		'declaration-no-important': null,
 		'selector-class-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$|^-([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+
+		'plugin/root-colors': true,
 	},
 	overrides: [
 		{
